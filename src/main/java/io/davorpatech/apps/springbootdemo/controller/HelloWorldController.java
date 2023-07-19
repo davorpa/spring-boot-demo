aded8522bc4ff19958d3f7cf5caa4b53854bcd29
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-07-19 17:51:25 +0200

    Agregar el HelloWorldController con un método

---
+++ src/main/java/io/davorpatech/apps/springbootdemo/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package io.davorpatech.apps.springbootdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping
+    public String index() {
+        return "Hello World!";
+    }
+}
