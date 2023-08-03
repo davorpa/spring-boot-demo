e6dc485cbb81c2cf8ab892a2be7aacb382619960
David Ordás <3125580+davorpa@users.noreply.github.com>
2023-08-03 19:46:17 +0200

    feat: add services ejercicio 04 taller agosto

---
+++ src/main/java/io/davorpatech/apps/springbootdemo/service/bootcamp/AlumnoService.java
@@ -0,0 +1,75 @@
+package io.davorpatech.apps.springbootdemo.service.bootcamp;
+
+import io.davorpatech.apps.springbootdemo.model.bootcamp.Alumno;
+import io.davorpatech.fwk.service.CrudEntityService;
+import org.springframework.lang.NonNull;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Servicio de básicos para la entidad de dominio {@link Alumno}.
+ */
+public interface AlumnoService // NOSONAR
+        extends CrudEntityService<Alumno, Long> // NOSONAR
+{
+    /**
+     * Busca un alumno por su clave natural
+     * (documento nacional de indentidad).
+     *
+     * @param nid número de documento nacional de indentidad del alumno,
+     *            nunca {@code null}.
+     * @return nunca {@code null}, {@literal Optional.empty()} si el
+     *         registro no existe.
+     */
+    Optional<Alumno> findByNid(@NonNull String nid);
+
+    /**
+     * Comprueba si un alumno existe dada su clave natural
+     * (documento nacional de indentidad).
+     *
+     * @param nid número de documento nacional de indentidad del alumno,
+     *            nunca {@code null}.
+     * @return @{@code true} si existe
+     */
+    boolean existsByNid(@NonNull String nid);
+
+    /**
+     * Busca todos los alumnos dadas sus claves naturales
+     * (documento nacional de indentidad).
+     * <p>
+     * Los {@literal nid} que no se hayan encontrado no se tendrán en cuenta
+     * para el retorno, es decir, el método podrá retornar menos resultados
+     * que {@literal nids} de entrada.
+     *
+     * @param nids números de documento nacional de indentidad de
+     *             los alumnos, nunca {@code null}.
+     * @return la lista de registros, nunca {@code null}.
+     */
+    List<Alumno> findAllByNid(@NonNull Iterable<String> nids);
+
+    /**
+     * Elimina un alumno por su clave natural
+     * (documento nacional de indentidad).
+     * <p>
+     * Si no se encuentra ningún alumno con dicho {@literal nid},
+     * el sistema no hace nada.
+     *
+     * @param nid número de documento nacional de indentidad del alumno,
+     *            nunca {@code null}.
+     */
+    void deleteByNid(@NonNull String nid);
+
+    /**
+     * Elimina alumnos por su clave natural
+     * (documento nacional de indentidad).
+     * <p>
+     * Aquellos registros que no se hayan podido encontrar por su {@literal nid},
+     * es como si ya no existieran, volviendo al flujo del programa
+     * silenciosamente.
+     *
+     * @param nids números de documento nacional de indentidad de los alumnos,
+     *            nunca {@code null}.
+     */
+    void deleteAllByNid(@NonNull Iterable<String> nids);
+}
---
+++ src/main/java/io/davorpatech/apps/springbootdemo/service/bootcamp/impl/AlumnoServiceImpl.java
@@ -0,0 +1,61 @@
+package io.davorpatech.apps.springbootdemo.service.bootcamp.impl;
+
+import io.davorpatech.apps.springbootdemo.model.bootcamp.Alumno;
+import io.davorpatech.apps.springbootdemo.persistence.bootcamp.AlumnoRepository;
+import io.davorpatech.apps.springbootdemo.service.bootcamp.AlumnoService;
+import io.davorpatech.fwk.service.AbstractCrudEntityService;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * Implementación del servicio de básicos para la entidad {@link Alumno}.
+ * <p>
+ * Se delega su operativa en el correspondiente
+ * {@link org.springframework.data.jpa.repository.JpaRepository JpaRepository}.
+ */
+@Service
+public class AlumnoServiceImpl
+        extends AbstractCrudEntityService<Alumno, Long>
+        implements AlumnoService
+{
+    private AlumnoRepository alumnoRepository;
+
+    public AlumnoServiceImpl(final AlumnoRepository alumnoRepository) {
+        this.alumnoRepository = Objects.requireNonNull(
+                alumnoRepository, "alumnoRepository must not be null!");
+    }
+
+    @Override
+    protected AlumnoRepository getRepository() {
+        return this.alumnoRepository;
+    }
+
+    @Override
+    public Optional<Alumno> findByNid(final @NonNull String nid) {
+        return alumnoRepository.findByNid(nid);
+    }
+
+    @Override
+    public boolean existsByNid(final @NonNull String nid) {
+        return alumnoRepository.existsByNid(nid);
+    }
+
+    @Override
+    public List<Alumno> findAllByNid(final @NonNull Iterable<String> nids) {
+        return alumnoRepository.findAllByNid(nids);
+    }
+
+    @Override
+    public void deleteByNid(final @NonNull String nid) {
+        alumnoRepository.deleteByNid(nid);
+    }
+
+    @Override
+    public void deleteAllByNid(final @NonNull Iterable<String> nids) {
+        alumnoRepository.deleteAllByNid(nids);
+    }
+}
